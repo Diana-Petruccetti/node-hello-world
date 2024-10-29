24db10ce22f8e1e2d90018a81ad790555808efb3
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-10-29 10:01:33 +0100

    exercise corrected

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 console.log('hello node js');
 
-process.env.PIN = 333;
+/* console.log(process); */
+
+const PIN = process.env.PIN;
+console.log(PIN);
+
+if (!PIN) {
+    console.error('Devi aggiungere una variabile PIN');
+    process.exit(1)
+}
+
+/* Leggi la value di un argument */
+/* console.log(process.argv); */
+const user = process.argv[2]
+console.log(user);
+
+if (!user) {
+    console.error('Devi aggiungere un argument user');
+    process.exit(1)
+}
+
+
+// BONUS
+
+if (user == 'admin' && PIN == 333) {
+    console.log('Benvenuto Admin');
+} else {
+    console.log('Accesso Negato');
+}
